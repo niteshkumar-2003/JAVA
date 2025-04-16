20963b4c423698fe8bd41d81a91715317d51fa24
niteshkumar-2003 <niteshkumary886@gmail.com>
2025-04-16 17:51:27 +0530

    Added

---
+++ input.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String s = sc.nextLine();
+        String s1 = sc.nextLine();
+        if(s.contains(s1))
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+        
+        
+    }
+}
